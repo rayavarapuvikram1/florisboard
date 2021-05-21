951f14514d23ed1e3650b7cabef1df3d4cc38b27
rayavarapuvikram1 <rayavarapuvikram1@gmail.com>
2021-05-21 19:01:08 +0530

    Added Project Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,22 @@ subprojects {
         jcenter() // Cannot remove jcenter as of now because flexbox depends on it
     }
 }
+buildscript {
+    repositories {
+        google()  // Google's Maven repository
+    }
+    dependencies {
+        // Add this line
+        classpath ("com.google.gms:google-services:4.3.8")
+    }
+}
 
+allprojects {
+    repositories {
+        // Check that you have the following line (if not, add it):
+        google()  // Google's Maven repository
+    }
+}
 // for dependency updates task, only display versions that aren't tagged with alpha or beta
 tasks.named<DependencyUpdatesTask>("dependencyUpdates").configure {
     rejectVersionIf {
